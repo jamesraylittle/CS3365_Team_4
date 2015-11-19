3467445ae780f047dbbe4d696d76e85e88fbbab8
Mose Gumble <mosegumble@gmail.com>
2015-11-18 22:17:17 -0600

    Pushing code for db

--- DMHelper4/app/src/main/java/group4/dmhelper/Activities/Popups/PopupEditHealth.java
+++ DMHelper4/app/src/main/java/group4/dmhelper/Activities/Popups/PopupEditHealth.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import group4.dmhelper.Actors.Actor;
 import group4.dmhelper.Fragments.FragmentFeed;
 import group4.dmhelper.R;
 
@@ -26,14 +27,16 @@ public class PopupEditHealth extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         Bundle extras = getIntent().getExtras();
-        final String PlayerIdentifier = extras.getString("Identifier");
-        //Actor player = new Actor(PlayerIdentifier);
+        final int PlayerIdentifier = extras.getInt("Identifier");
+
+
         WindowManager.LayoutParams windowManager = getWindow().getAttributes();
         windowManager.dimAmount = 0.5f;
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
         int width = dm.widthPixels;
         int height = dm.heightPixels;
         getWindow().setLayout((int) (width * 0.7), (int) (height * 0.3));
+
         Button submit = (Button) findViewById(R.id.btn_submit_health);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,10 +47,11 @@ public class PopupEditHealth extends AppCompatActivity {
                 if(text != "") {
                     int health = Integer.parseInt(text);
                     //TODO EDIT ACTOR HEALTH
-
-                    //player.setHealth(player.getHealth() + health)
+                    Actor player = new Actor(PlayerIdentifier,getApplicationContext());
+                    player.setHealth(player.getHealth() + health);
                     FragmentFeed.feedItems.add(PlayerIdentifier + " edited health by: " + health); //TODO ADD PLAYER NAME
                     Toast.makeText(getApplicationContext(),PlayerIdentifier + " edited health by: " + health, Toast.LENGTH_LONG).show();
+                    player.pushToDatabase();
                     finish();
                 }
                 else {
